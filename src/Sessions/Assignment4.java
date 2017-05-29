f3c44f4b7febb156b248ae52d27a841882e48af8
pgsr91 <pgsr91@hotmail.com>
2017-05-29 20:05:43 +0530

    Assignment 1.4

---
+++ src/Sessions/Assignment4.java
@@ -0,0 +1,14 @@
+package Sessions;
+
+public class Assignment4  //main class
+{
+	public static void main(String arg[])  
+     {
+			
+		for(int i=65;i<=90;i++)  //	Iteration statement to print values repeatedly.
+ {
+	 char a = (char) i; //Converting integer variable to character type. (type casting)
+	 System.out.println(i+"-"+a); // Printing the ACCII values in desired format.
+ }
+     }
+} 
